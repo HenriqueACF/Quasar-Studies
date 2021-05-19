a3461d18ae6cf08532a96c183df6dd4e90fb30bd
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2021-05-19 14:42:28 -0300

    adicionando um novo layout e passando uma rota

--- teste/src/router/routes.js
+++ teste/src/router/routes.js
@@ -5,9 +5,20 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      {path:'perfil', component:()=> import('pages/Perfil.vue')}
+      // caso a rota perfil utilizasse o mesmo layout 
+      //{path:'perfil', component:()=> import('pages/Perfil.vue')}
     ]
   },
+
+  //Criando um novo objeto para o novo layout
+  {
+    path:'/perfil',
+    component:() =>import('layouts/LayoutPerfil.vue'),
+    children:[
+      {path:'', component:()=>import('pages/Perfil.vue')}
+    ]
+  },
+
 
   // Always leave this as last one,
   // but you can also remove it
