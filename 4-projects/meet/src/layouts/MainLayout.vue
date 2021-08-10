35b8ca3721d4a55ab05cf7339076510932aae0f5
HenriqueACF <henrique.assis145@gmail.com>
2021-08-10 15:33:25 -0300

    meet-quasar

---
+++ 4-projects/meet/src/layouts/MainLayout.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="row justify-between">
+        <q-img
+          src="../assets/google-meet-logo.png"
+          width="200px"
+          class="q-ma-md"
+        >
+        </q-img>
+        <div class="row">
+          <div class="q-mr-md">
+            <q-icon
+              name="help_outline"
+              size="25px"
+              color="grey-7"
+              class="q-my-sm q-mx-md"
+            />
+            <q-icon
+              name="sms_failed"
+              size="25px"
+              color="grey-7"
+              class="q-my-sm q-mx-md"
+            />
+            <q-icon
+              name="settings"
+              size="25px"
+              color="grey-7"
+              class="q-my-sm q-mx-md"
+            />
+          </div>
+          <div>
+            <q-icon
+              name="apps"
+              size="25px"
+              color="grey-7"
+              class="q-my-sm q-mx-md"
+            />
+            <q-avatar class="q-mr-sm">
+              <img src="https://cdn.quasar.dev/img/avatar.png" />
+            </q-avatar>
+          </div>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {},
+});
+</script>
---
+++ 4-projects/meet/src/pages/Index.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-page class="row justify-around full-width">
+    <div style="width:50%" class="column q-pa-xl justify-center">
+      <h3 class="q-ma-none">Video chamadas premium.</h3>
+      <h3 class="q-ma-none q-mx-none">Agora gratuita para todos.</h3>
+
+      <span style="font-size:20px; color:#5F6368">Reformulamos o Google Meet, nosso serviço seguro para reuniões de negocios. Agora ela é aberta pra todos!</span>
+      <div class="row" style="margin:70px 0px;">
+        <q-btn color="secondary" icon="video_call" label="Nova Reunião" class="q-mr-lg" />
+        <q-input outlined v-model="text" :dense="dense" label="Digite um código ou link" style="width:300px">
+          <template v-slot:prepend>
+            <q-icon name="keyboard"/>
+          </template>
+        </q-input>
+      </div>
+
+      <q-separator class="q-mb-xl"></q-separator>
+      
+      <span style="font-size:18px; color:#5F6368">
+        <a href="" style="color:#056B5F; text-decoration:none">Saiba mais</a> sobre o Google Meeet.
+      </span>
+    </div>
+    <div style="width:50%" class="row items-center justify-center">
+      <q-card>
+        <q-card-section class="my-card">
+          <q-img src="../assets/reuniao.png" width="600px"></q-img>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  data(){
+    return{
+      text:'',
+      dense:false
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.my-card{
+  .q-img{
+    border-radius: 7px;
+  }
+}
+</style>
