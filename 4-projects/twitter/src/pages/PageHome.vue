32de341f995a44076049b0872e41d5efb0d83248
henriqueacf <henrique.assis145@gmail.com>
2021-12-12 19:44:02 -0300

    feat final design touches

--- 4-projects/twitter/src/pages/PageHome.vue
+++ 4-projects/twitter/src/pages/PageHome.vue
@@ -1,83 +1,106 @@
 <template>
-  <q-page>
-    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
-      <div class="col">
-        <q-input
-          bottom-slots
-          v-model="newQTweetContent"
-          class="new-qtweet"
-          label="What's happening?"
-          counter
-          maxlength="280"
-          autogrow
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute fullscreen">
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <div class="col">
+          <q-input
+            bottom-slots
+            v-model="newQTweetContent"
+            class="new-qtweet"
+            label="What's happening?"
+            counter
+            maxlength="280"
+            autogrow
+          >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img
+                  src="https://avatars.githubusercontent.com/u/45006908?v=4"
+                />
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+        <div class="col col-shrink">
+          <q-btn
+            @click="addNewQweet"
+            class="q-mb-lg"
+            :disable="!newQTweetContent"
+            unelevated
+            rounded
+            color="primary"
+            label="QTweet"
+            no-caps
+          />
+        </div>
+      </div>
+
+      <q-separator class="divider" size="10px" color="grey-2" />
+
+      <q-list separator>
+        <transition-group
+          appear
+          enter-active-class="animated fadeIn slower"
+          leave-active-class="animated fadeOut slower"
         >
-          <template v-slot:before>
-            <q-avatar size="xl">
-              <img src="https://avatars.githubusercontent.com/u/45006908?v=4" />
-            </q-avatar>
-          </template>
-        </q-input>
-      </div>
-      <div class="col col-shrink">
-        <q-btn
-          @click="addNewQweet"
-          class="q-mb-lg"
-          :disable="!newQTweetContent"
-          unelevated
-          rounded
-          color="primary"
-          label="QTweet"
-          no-caps
-        />
-      </div>
-    </div>
+          <q-item
+            class="qweet q-py-md"
+            v-for="qweet in qweets"
+            :key="qweet.date"
+          >
+            <q-item-section avatar top>
+              <q-avatar size="xl">
+                <img
+                  src="https://avatars.githubusercontent.com/u/45006908?v=4"
+                />
+              </q-avatar>
+            </q-item-section>
 
-    <q-separator class="divider" size="10px" color="grey-2" />
+            <q-item-section>
+              <q-item-label class="text-subtitle1">
+                <strong>Henrique Assis</strong>
+                <span class="text-grey-7">
+                  @henriqueacf
+                  <!-- <br class="lt-md"/> &bull:{{ qweet.date | relativeDate }} -->
+                </span>
+              </q-item-label>
+              <q-item-label class="qweet-content text-body1">
+                {{ qweet.content }}
+              </q-item-label>
+              <div class="qweet-icons row justify-between q-mt-sm">
+                <q-btn
+                  color="grey"
+                  icon="far fa-comment"
+                  size="sm"
+                  flat
+                  round
+                />
+                <q-btn
+                  color="grey"
+                  icon="fas fa-retweet"
+                  size="sm"
+                  flat
+                  round
+                />
+                <q-btn color="grey" icon="far fa-heart" size="sm" flat round />
+                <q-btn
+                  @click="deleteQweet(qweet)"
+                  color="grey"
+                  icon="fas fa-trash"
+                  size="sm"
+                  flat
+                  round
+                />
+              </div>
+            </q-item-section>
 
-    <q-list separator>
-      <transition-group
-        appear
-        enter-active-class="animated fadeIn slower"
-        leave-active-class="animated fadeOut slower"
-      >
-        <q-item class="qweet q-py-md" v-for="qweet in qweets" :key="qweet.date">
-          <q-item-section avatar top>
-            <q-avatar size="xl">
-              <img src="https://avatars.githubusercontent.com/u/45006908?v=4" />
-            </q-avatar>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>
-              <strong class="text-subtitle1 text-weight-bolder"
-                >Henrique Assis</strong
-              >
-              <span class="text-grey-7"> @henriqueacf </span>
-            </q-item-label>
-            <q-item-label class="qweet-content text-body1">
-              {{ qweet.content }}
-            </q-item-label>
-            <div class="qweet-icons row justify-between q-mt-sm">
-              <q-btn color="grey" icon="far fa-comment" size="sm" flat round />
-              <q-btn color="grey" icon="fas fa-retweet" size="sm" flat round />
-              <q-btn color="grey" icon="far fa-heart" size="sm" flat round />
-              <q-btn
-                @click="deleteQweet(qweet)"
-                color="grey"
-                icon="fas fa-trash"
-                size="sm"
-                flat
-                round
-              />
-            </div>
-          </q-item-section>
-
-          <q-item-section side top>
-            {{ qweet.date | relativeDate }}
-          </q-item-section>
-        </q-item>
-      </transition-group>
-    </q-list>
+            <q-item-section side top>
+              {{ qweet.date | relativeDate }}
+            </q-item-section>
+          </q-item>
+        </transition-group>
+      </q-list>
+    </q-scroll-area>
   </q-page>
 </template>
 
@@ -146,7 +169,7 @@ export default {
   border-color: $grey-4
 
 .qweet:not(:first-child)
-  border-top:1px solid rgba(0,0,0,0.12)
+  border-top: 1px solid rgba(0,0,0,0.12)
 
 .qweet-content
   white-space: pre-line
