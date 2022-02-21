f9512e051946691f3ee04240b9f54020f95e984e
henriqueacf <henrique.assis145@gmail.com>
2022-02-21 19:26:25 -0300

    feat: add some pages and autentication

--- quasar-supabase/src/boot/supabase.js
+++ quasar-supabase/src/boot/supabase.js
@@ -1,8 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
+import useAuthUser from 'src/composables/UseAuthUser'
 
 const supabaseUrl = 'https://lnvfueakvdjxpmsldiql.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxudmZ1ZWFrdmRqeHBtc2xkaXFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU0NDg3MTAsImV4cCI6MTk2MTAyNDcxMH0.spmfPc27Kwbz7Zo44BV2vUrkepk7RSfjxLx7Dj2ekvE'
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+supabase.auth.onAuthStateChange((event, session) => {
+  const { user } = useAuthUser()
+
+  user.value = session?.user || null
+})
 
 console.log('supabase On', supabase)
 
---
+++ quasar-supabase/src/pages/EmailConfirmation.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page padding>
+    <p class='text-body1'>Thanks for registering!</p>
+    <p class='text-body1'>Please confirm your email form finishing registering: <strong>{{ $route.query.email }}</strong></p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageEmailConfirmation'
+})
+</script>
