7e595d6824508e52fc1c5d9cf3cab858dda86830
henriqueacf <henrique.assis145@gmail.com>
2022-02-22 10:43:11 -0300

    feat: logout && rotas de segurança

--- quasar-supabase/src/layouts/MainLayout.vue
+++ quasar-supabase/src/layouts/MainLayout.vue
@@ -15,7 +15,15 @@
           Quasar App
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn-dropdown flat color="white" icon="person">
+          <q-list>
+            <q-item clickable v-close-popup @click='handleLogout'>
+              <q-item-section>
+                <q-item-label>Logout</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
 
@@ -47,6 +55,9 @@
 
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
+import useAuthUser from 'src/composables/UseAuthUser'
+import { useRouter } from 'vue-router'
+import { useQuasar } from 'quasar'
 
 const linksList = [
   {
@@ -105,12 +116,31 @@ export default defineComponent({
   setup () {
     const leftDrawerOpen = ref(false)
 
+    const $q = useQuasar()
+
+    const router = useRouter()
+
+    const { logout } = useAuthUser()
+
+    const handleLogout = async () => {
+      $q.dialog({
+        title: 'Logout',
+        message: 'Do you really want to leave?',
+        cancel: true,
+        persistent: true
+      }).onOk(async () => {
+        await logout()
+        router.replace({ name: 'login' })
+      })
+    }
+
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      },
+      handleLogout
     }
   }
 })
