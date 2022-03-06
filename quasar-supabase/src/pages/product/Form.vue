4d871b19ff226a0142c274904ba4cb1405f1ccee
henriqueacf <henrique.assis145@gmail.com>
2022-03-06 11:38:58 -0300

    fix: list categories in the form

--- quasar-supabase/src/pages/product/Form.vue
+++ quasar-supabase/src/pages/product/Form.vue
@@ -77,6 +77,7 @@ import { defineComponent, ref, onMounted, computed } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import useApi from 'src/composables/UseApi'
 import useNotify from 'src/composables/UseNotify'
+import useAuthUser from 'src/composables/UseAuthUser'
 
 export default defineComponent({
   name: 'PageFormCategory',
@@ -84,8 +85,9 @@ export default defineComponent({
     const table = 'product'
     const router = useRouter()
     const route = useRoute()
-    const { post, getById, update, list, uploadImg } = useApi()
+    const { post, getById, update, listPublic, uploadImg } = useApi()
     const { notifyError, notifySuccess } = useNotify()
+    const { user } = useAuthUser()
 
     const isUpdate = computed(() => route.params.id)
 
@@ -109,7 +111,7 @@ export default defineComponent({
     })
 
     const handleListCategories = async () => {
-      optionsCategory.value = await list('category')
+      optionsCategory.value = await listPublic('category', user.value.id)
     }
 
     const handleSubmit = async () => {
