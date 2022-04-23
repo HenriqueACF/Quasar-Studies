2dd331939731e6fb4aa5140207f0a8001b433560
henriqueacf <henrique.assis145@gmail.com>
2022-04-23 10:15:13 -0300

    feat [pageHome]: paina de posts criada

--- quasar-instagram/src/pages/PageHome.vue
+++ quasar-instagram/src/pages/PageHome.vue
@@ -1,37 +1,59 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card
-        class="card-post q-mb-md"
-        flat bordered
-        v-for="post in posts"
-        :key="post.id"
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://github.com/henriqueacf.png">
-          </q-avatar>
-        </q-item-section>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+            class="card-post q-mb-md"
+            flat bordered
+            v-for="post in posts"
+            :key="post.id"
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://github.com/henriqueacf.png">
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-bold">Henrique_Assis</q-item-label>
-          <q-item-label caption>
-            {{post.location}}
-          </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label class="text-bold">Henrique_Assis</q-item-label>
+              <q-item-label caption>
+                {{post.location}}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator />
+          <q-separator />
 
-      <q-img
-          :src="post.imageUrl"
-      />
+          <q-img
+              :src="post.imageUrl"
+          />
 
-      <q-card-section>
-        <div class="">{{post.caption}}</div>
-        <div class="text-caption text-grey">{{post.date}}</div>
-      </q-card-section>
-    </q-card>
+          <q-card-section>
+            <div class="">{{post.caption}}</div>
+            <div class="text-caption text-grey">{{post.data}}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://github.com/henriqueacf.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">Henrique_Assis</q-item-label>
+            <q-item-label caption>
+              Henrique
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
+
   </q-page>
 </template>
 
@@ -45,27 +67,32 @@ export default defineComponent({
       posts: [
         {
           id: 1,
-          caption: 'Golden Gate Bridge',
-          date: 1650672190001,
+          caption: 'Legenda foto 1',
+          data: 1650672190001,
           location: 'Belém',
           imageUrl: 'https://picsum.photos/200/300'
         },
         {
           id: 2,
-          caption: 'Golden Gate Bridge',
-          date: 1650672190001,
+          caption: 'Legenda foto 2',
+          data: 1650672190001,
           location: 'Belém',
           imageUrl: 'https://picsum.photos/200'
         },
         {
           id: 3,
-          caption: 'Golden Gate Bridge',
-          date: 1650672190001,
+          caption: 'Legenda foto 3',
+          data: 1650672190001,
           location: 'Belém',
           imageUrl: 'https://picsum.photos/seed/picsum/200/300'
         }
       ]
     }
+  },
+  methods: {
+    // formatDate (post.data) {
+    //   return date.formatDate(post.data 'DD/MM/YYYY')
+    // }
   }
 })
 </script>
