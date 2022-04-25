c522e59754c286ab4783c2a9de6f9c3faa6e6725
henriqueacf <henrique.assis145@gmail.com>
2022-04-25 17:10:03 -0300

    feat [PageCamera]: inicio da criação da função de captura de tela

--- quasar-instagram/src/pages/PageCamera.vue
+++ quasar-instagram/src/pages/PageCamera.vue
@@ -1,17 +1,45 @@
 <template>
 <q-page class="constrain-more q-pa-md">
   <div class="camera-frame q-pa-md">
-    <img
-        class="full-width"
-        src="https://picsum.photos/seed/picsum/200/200"/>
+<!--    <img-->
+<!--        class="full-width"-->
+<!--        src="https://picsum.photos/seed/picsum/200/200"/>-->
+    <video
+        v-show="!imageCaptured"
+      ref="video"
+      class="full-width"
+      autoplay
+    />
+    <canvas
+        v-show="imageCaptured"
+      ref="canvas"
+      class="full-width"
+      height="240"
+    />
   </div>
   <div class="text-center q-pa-md">
     <q-btn
+        v-if="hasCameraSupport"
+        @click="captureImage"
       color="grey-9"
       icon="eva-camera"
       size="lg"
       round
     />
+    <q-file
+      v-else
+      v-model="imageUpload"
+      @input="captureImageFallBack"
+      label="Choose an image"
+      accept="image/*"
+      outlined
+    >
+      <template
+        v-slot:prepend
+      >
+        <q-icon name="eva-attach-outline"/>
+      </template>
+    </q-file>
     <div class="row justify-center q-ma-md">
       <q-input
           v-model="post.caption"
@@ -51,6 +79,7 @@
 
 <script>
 import { uid } from 'quasar'
+require('md-gum-polyfill')
 
 export default {
   name: 'PageCamera',
@@ -62,8 +91,56 @@ export default {
         location: '',
         photo: null,
         date: Date.now()
-      }
+      },
+      imageCaptured: false,
+      imageUpload: [],
+      hasCameraSupport: true
     }
+  },
+  methods: {
+    initCamera () {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true
+        })
+        .then(stream => {
+          this.$refs.video.srcObject = stream
+        })
+        .catch(error => {
+          this.hasCameraSupport = false
+        })
+    },
+    captureImage () {
+      // console.log('capture Image')
+      const video = this.$refs.video
+      const canvas = this.$refs.canvas
+      canvas.width = video.getBoundingClientRect().width
+      canvas.height = video.getBoundingClientRect().height
+      const context = canvas.getContext('2d')
+      context.drawImage(video, 0, 0, canvas.width, canvas.height)
+      this.imageCaptured = true
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL())
+    },
+    captureImageFallBack (file) {
+      console.log('file', file)
+      this.post.photo = file
+    },
+    dataURItoBlob (dataURI) {
+      const byteString = atob(dataURI.split(',')[1])
+      const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+      const ab = new ArrayBuffer(byteString.length)
+      const ia = new Uint8Array(ab)
+      for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i)
+      }
+      const blob = new Blob([ab], {
+        type: mimeString
+      })
+      return blob
+    }
+  },
+  mounted () {
+    this.initCamera()
   }
 }
 </script>
